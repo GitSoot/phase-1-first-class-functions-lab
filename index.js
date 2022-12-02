4ac3cbb59e6b2e23d908942a5547d95012264be1
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-12-02 13:15:52 +0300

    Completed Assignment,

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function afterEach() {
+    return drivers
+}
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  };
+  
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+ function createFareMultiplier(multiplyfare) {
+    return function (value) {
+      return multiplyfare * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+
+  const selectDifferentDrivers = (drivers, returnDrivers) => returnDrivers(drivers);
